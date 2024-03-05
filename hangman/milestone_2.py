26804c52f264a3ae4b929d4e6e3beb6178c8de4d
ArjunChe <162313265+ArjunChe@users.noreply.github.com>
2024-03-05 11:31:45 +0000

    Update milestone_2.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -1,6 +1,9 @@
 import random
 word_list = ['apple', 'banana', 'orange', 'pear', 'strawberry']
 print(word_list)
-
 word = random.choice(mylist)
+
+guess = input("Please enter a single letter:")
+
+
 
