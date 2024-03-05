14fc43eff2e3b1e533192128c1e567f7fd0bc18e
ArjunChe <162313265+ArjunChe@users.noreply.github.com>
2024-03-05 15:41:34 +0000

    Update milestone3.py

--- hangman/milestone3.py
+++ hangman/milestone3.py
@@ -1,1 +1,17 @@
+def check_guess(guess):
+  lc_guess = guess.lower()
+  if lc_guess in word:
+    print (f"Good guess! {lc_guess} is in the word.")
+  else:
+    print (f"Sorry, {lc_guess} is not in the word. Try again.")
+
+def ask_for_input():
+  return check_guess('A')
+  while True:
+    guess = input("Please enter a single letter:")
+    if guess.isalpha() and len(guess) == 1:
+      break
+    else:
+      print("Invalid letter. Please, enter a single alphabetical character.")
+      
 
