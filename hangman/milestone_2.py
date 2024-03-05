fcb0677b637014fdd6eef31021dd34a347bddf8a
ArjunChe <162313265+ArjunChe@users.noreply.github.com>
2024-03-05 11:35:51 +0000

    Update milestone_2.py

--- hangman/milestone_2.py
+++ hangman/milestone_2.py
@@ -4,6 +4,10 @@ print(word_list)
 word = random.choice(mylist)
 
 guess = input("Please enter a single letter:")
+if guess.isalpha() and len(guess) == 1:
+  print ("Good Guess!")
+else:
+  print ("Oops! That is not a valid input.")
 
 
 
